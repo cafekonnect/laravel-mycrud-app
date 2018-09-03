765856e91a7ae8614930216d84364224d1a659b5
christopher Mulei <diligentmulei@gmail.com>
2018-09-03 13:07:17 +0300

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-center">
+  <h1>Welcome to my laravel website</h1>
+<p>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, facere. Saepe sunt, cum numquam dolorum itaque nemo earum expedita ipsa neque vel at, officia perspiciatis quae quisquam labore. Quibusdam, rerum?
+</p>
+<p><a class="btn btn-primary btn-lg" href="/register" role="button">Register &raquo;</a>
+<p><a class="btn btn-primary btn-lg" href="/login" role="button">Login &raquo;</a>
+ 
+</p>
+</div>
+</div>       
+
+
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+<br><br>
+
+@section('content')
+<br>
+<a href="/posts" class="btn btn-default">Go Back</a>
+<h1>{{$post->title}}</h1>
+<br>
+<div>
+    {{$post->body}}
+</div>
+<hr>
+<small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+<hr>
+<a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+{{!!Form::open(['action' =>['PostsController@destroy',$post->id],'method' =>'POST', 'class' => 'pull-right'])!!}}
+{{Form::hidden('_method','DELETE')}}
+{{Form::submit('Delete',['class'=> 'btn btn-danger'])}}
+{!!Form::close()!!}
+@endsection
+
